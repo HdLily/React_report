25d6f67c01078ecb9fdb1bc6e634ac01696cce89
HdLily <2162221525@qq.com>
2018-07-19 14:18:39 +0800

    第一次提交

---
+++ ryf-react/src/App5.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LikeButton extends Component {
+    state = {
+        liked: false
+    }
+    handleClick () {
+        // console.log(this)
+        this.setState({
+            liked: !this.state.liked
+        })
+    }
+    render() {
+        const text = this.state.liked? 'like':'don\'t like'
+        return (
+            <p onClick={this.handleClick.bind(this)}> You {text} this.click to toggle</p>
+        )
+    }
+}
+class App extends Component {
+    handleClick () {
+        return (
+            this.refs.myTextInput.focus()
+        )
+    }
+    render() {
+
+        return (
+            <div className="App">
+                <LikeButton text="haha"/>
+                <input type="text" ref="myTextInput"/>
+                <input type="button" value="Focus the text input" onClick={this.handleClick.bind(this)} />
+
+            </div>
+        );
+    }
+}
+
+export default App;
